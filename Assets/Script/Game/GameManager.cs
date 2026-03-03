cb4123543661fe2991d3b7051738b1f959194483
nelu572 <s25031@gsm.hs.kr>
2026-03-03 12:29:42 +0900

    캐릭터 선택, 게임시작 구현

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -8,15 +8,10 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        CharacterSelectData.player1Index = 0;
-        CharacterSelectData.player2Index = 0;
-
         GameObject p1 = SpawnPlayer(CharacterSelectData.player1Index, "Player1", new Vector3(-3, 0, 0));
         GameObject p2 = SpawnPlayer(CharacterSelectData.player2Index, "Player2", new Vector3(-3, 0, 0));
 
-
         cameraMove.SetTargets(p1.transform, p2.transform);
-
     }
 
     GameObject SpawnPlayer(int index, string actionMap, Vector3 pos)
