f9f33ae8273e972e754bf2a3bb0c693bd1b07672
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-04-29 10:29:06 +0900

    fix: ExampleScript 필드 추가 & 로직 수정
    #6

--- Assets/Script/Game/ExampleScript.cs
+++ Assets/Script/Game/ExampleScript.cs
@@ -4,10 +4,15 @@ public class ExampleScript : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _enemy;
+    private IDamageable _playerDamageable;
+    private IDamageable _enemyDamageable;
 
     void Awake()
     {
-        _player.GetComponent<IDamageable>().TeamId = 1;
-        _enemy.GetComponent<IDamageable>().TeamId = 2;
+        _playerDamageable = _player.GetComponent<IDamageable>();
+        _enemyDamageable = _enemy.GetComponent<IDamageable>();
+
+        _playerDamageable.TeamId = 1;
+        _enemyDamageable.TeamId = 2;
     }
 }
