f85be44b0426388430db836f2ab21b9f9bf2c173
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-04-29 09:22:18 +0900

    fix: 방어력 제거 & float -> int로 변경
    #6

--- Assets/Script/Game/Player/PlayerStat.cs
+++ Assets/Script/Game/Player/PlayerStat.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 public class PlayerStat : MonoBehaviour, IDamageable
 {
     public float Hp { get; set; }
-    private float _maxHp;
-    public float MaxHp => _maxHp;
-    public float Defence { get; set; } = 30f;
-    public float AttackDamage { get; set; } = 50f;
-    public float SkillDamage { get; set; }
+    private int _maxHp;
+    public int MaxHp => _maxHp;
+    public int AttackDamage { get; set; }
+    public int SkillDamage { get; set; }
     public float SkillCooltime { get; set; }
     public float AttackCooltime { get; set; }
     public int TeamId { get; set; }
 
     public void TakeDamage(int attackDamage)
     {
-        Hp -= (Defence - attackDamage);
+        Hp -= attackDamage;
     }
 }
