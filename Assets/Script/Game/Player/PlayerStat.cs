c5610d7cdb53493d1373398d87ba05cf4ca0dc50
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-04-30 15:11:24 +0900

    feat: 목숨 프로퍼티 추가 & 체력 감소 로직 수정
    #6

--- Assets/Script/Game/Player/PlayerStat.cs
+++ Assets/Script/Game/Player/PlayerStat.cs
@@ -3,6 +3,13 @@
 
 public class PlayerStat : MonoBehaviour, IDamageable
 {
+
+    ///TODO InGameManager에서 부활 함수 작성해야됨
+    ///부활 함수는 체력이 0일때 실행하는 함수이며 Life를 1만큼 감소하고
+    ///Hp값을 MaxHp값으로 대입 
+    ///그후 죽은 캐릭터는 다시 움직일 수 있게 만들어야됨(이건 어떤 스크립트를 만들어야되지)
+    //이 프로퍼티는 목숨입니다
+    public int Life { get; set; } = 3;
     public int Hp { get; set; }
     private int _maxHp;
     public int MaxHp { get => _maxHp; set => _maxHp = value; }
@@ -15,5 +22,7 @@ public class PlayerStat : MonoBehaviour, IDamageable
     public void TakeDamage(int attackDamage)
     {
         Hp -= attackDamage;
+
+        if(Hp < 0) Hp = 0;
     }
 }
