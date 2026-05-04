428deb382bc709393a01699330f6464945fd29b4
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-04 09:09:55 +0900

    feat: 차징 컨트롤러와 차징 인터페이스 생성
    #24

---
+++ Assets/Script/Game/Player/ChargeInputHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChargeInputHandler : MonoBehaviour
+{
+    private IChargeable _chargeable;
+    private InputAction _attackAction;
+
+    void Awake()
+    {
+        _chargeable = GetComponent<IChargeable>();
+
+        // PlayerInput에서 Attack 액션 직접 가져오기
+        _attackAction = GetComponent<PlayerInput>().actions["Attack"];
+    }
+    void OnEnable()
+    {
+        _attackAction.canceled += OnAttack;
+    }
+
+    void OnDisable()
+    {
+        _attackAction.canceled -= OnAttack;
+    }
+
+    // 공격키 해재시 발동
+    public void OnAttack(InputAction.CallbackContext context)   
+    {
+        if (context.canceled)
+            _chargeable.ReleaseAttack();
+    }
+}
---
+++ Assets/Script/Game/Player/Interface/IChargeable.cs
@@ -0,0 +1,4 @@
+public interface IChargeable
+{
+    public void ReleaseAttack();
+}
