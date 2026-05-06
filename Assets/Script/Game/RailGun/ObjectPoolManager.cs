45214a90774ade95b6539b25f09db18db37118b1
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-06 17:33:01 +0900

    fix: 오브젝트 폴링 수정
    #22

---
+++ Assets/Script/Game/RailGun/ObjectPoolManager.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class PoolEntry
+    {
+        public string key;
+        public GameObject prefab;
+        public int initialSize = 3;
+    }
+    //미리 생성할 오브젝트들
+    [SerializeField] private List<PoolEntry> _entries;
+
+    // 키값으로 Queue를 구별하는 딕셔너리 생성
+    private Dictionary<string, Queue<GameObject>> _pools = new Dictionary<string, Queue<GameObject>>();
+
+    //키값으로 미리 생성한 오브젝트들 구별 & 관리하는 딕셔너리 생성
+    private Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
+
+    private void Awake()
+    {
+        //미리 생성할 오브젝트
+        foreach (var entry in _entries)
+        {
+            _pools[entry.key] = new Queue<GameObject>();
+            _prefabs[entry.key] = entry.prefab;
+            //오브젝트 생성
+            for (int i = 0; i < entry.initialSize; i++)
+                _pools[entry.key].Enqueue(CreateNew(entry.key));
+        }
+    }
+
+    public GameObject Get(string key)
+    {
+        if (!_pools.ContainsKey(key))
+        {
+            Debug.LogError($"[Pool] 키 없음: {key}");
+            return null;
+        }
+
+        var obj = _pools[key].Count > 0
+            ? _pools[key].Dequeue()
+            : CreateNew(key);
+
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void Return(string key, GameObject obj)
+    {
+        if (!_pools.ContainsKey(key))
+        {
+            Debug.LogWarning($"[Pool] 키 없음: {key}");
+            Destroy(obj);
+            return;
+        }
+
+        obj.SetActive(false);
+        _pools[key].Enqueue(obj);
+    }
+
+    private GameObject CreateNew(string key)
+    {
+        var obj = Instantiate(_prefabs[key]);
+        obj.SetActive(false);
+        return obj;
+    }
+}
--- Assets/Script/Game/RailGun/RailGunAttackController.cs
+++ Assets/Script/Game/RailGun/RailGunAttackController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerController))]
 [RequireComponent(typeof(PlayerStat))]
 [RequireComponent(typeof(ChargeInputHandler))]
-[RequireComponent(typeof(CreateLaser))]
+[RequireComponent(typeof(ObjectPoolManager))]
 public class RailGunAttackController : MonoBehaviour, IAttackController, IChargeable
 {
     [Header("일반 공격")]
@@ -28,6 +28,9 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
     [Header("레이저")]
     [SerializeField] private GameObject _laser;
     [SerializeField] private float _laserDuration = 0.5f;
+    // 오브젝트 폴링에 넘길 key값
+    // 이 오브젝트로 생성할 오브젝트를 찾은후 오브젝트 활성화
+    [SerializeField] private string _laserKey;
     private bool _laserActive = false;
     private float _laserTimer = 0f;
 
@@ -41,12 +44,12 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
     //쿨타임 공격력 가져오는 참조 변수
     private PlayerStat _playerStat;
     //레이저 생성 클래스
-    private CreateLaser _createLaser;
+    private ObjectPoolManager _objectPoolManager;
     // 애니메이션이 생긴다면 주석처리를 해제할 것입니다
     //private Animator _animator;
 
     private float _chargeTimer = 0f;
-    private bool _isCharging = false;
+    public bool IsCharging { get; set; }
     private Transform _detectedTarget = null;
 
     private float _skillChargeTimer = 0f;
@@ -59,7 +62,7 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
     void Awake()
     {
 
-        _createLaser = GetComponent<CreateLaser>();
+        _objectPoolManager= GetComponent<ObjectPoolManager>();
         _playerController = GetComponent<PlayerController>();
         _playerStat = GetComponent<PlayerStat>();
 
@@ -80,14 +83,14 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
 
         if(_laserTimer >= _laserDuration)
         {
-            _createLaser.Return(_laser); // 반납
+            _objectPoolManager.Return(_laserKey, _laser); // 반납
             _laser = null;//레이저 초기화
             _laserActive = false;
             _laserTimer = 0f;
         }
 
         // 일반 차징
-        if (_isCharging)
+        if (IsCharging)
         {
             _chargeTimer = Mathf.Clamp(_chargeTimer + Time.deltaTime, 0f, _maxCharge);
             _detectedTarget = DetectEnemy(_attackSize.x, _maxWidth, _attackSize.y, _maxCharge, _chargeTimer);
@@ -107,24 +110,24 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
         if (_playerStat.AttackCooltime > 0f) return;
 
         //if (_playerController.IsGrounded)
-            //_animator.SetTrigger("NormalAttack");
+        //_animator.SetTrigger("NormalAttack");
         //else
-            //_animator.SetTrigger("JumpAttack");
+        //_animator.SetTrigger("JumpAttack");
 
-        _isCharging = true;
+        IsCharging = true;
         _chargeTimer = 0f;
     }
 
     private void ReleaseAttack()
     {   //공격키 해제시 호출됨
-        if (!_isCharging) return;
+        if (!IsCharging) return;
 
         if (_playerController.IsGrounded)
             NormalAttack();
         else
             JumpAttack();
 
-        _isCharging = false;
+        IsCharging = false;
         _chargeTimer = 0f;
         _detectedTarget = null;
         _playerStat.AttackCooltime = _playerStat.AttackCooltimeMax;
@@ -290,7 +293,8 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
         // 스프라이트 중심이 origin에서 절반 길이만큼 앞에 오도록
         Vector2 spawnPos = origin + direction * (sizeX / 2f);
 
-        _laser = _createLaser.GetObject();
+        //레이저 키값을 통해 레이저 오브젝트 활성화
+        _laser = _objectPoolManager.Get(_laserKey);
         _laserActive = true;
         _laser.SetActive(_laserActive);// 레이저 오브젝트 활성화
         _laserTimer = 0f;
@@ -313,7 +317,7 @@ public class RailGunAttackController : MonoBehaviour, IAttackController, ICharge
         Vector2 facing = GetFacingDirection();
 
         // 일반 공격 감지 범위 (노란색)
-        if (_isCharging)
+        if (IsCharging)
         {
             float chargeRatio = _chargeTimer / _maxCharge;
             float attackSizeY = Mathf.Lerp(_maxWidth, _attackSize.y, chargeRatio); // 감지 범위 y값 축소
