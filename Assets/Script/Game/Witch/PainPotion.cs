72c4140c3a3c1c6d3ef83a9aac08cc9d2127bafb
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-07 09:15:39 +0900

    fix: 포션 스크립트 이름 변경
    #22

--- Assets/Script/Game/Witch/PainPotion.cs
+++ Assets/Script/Game/Witch/PainPotion.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PainPosion : MonoBehaviour
+public class PainPotion : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Script/Game/Witch/PoisonPotion.cs
+++ Assets/Script/Game/Witch/PoisonPotion.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Posion1 : MonoBehaviour
+public class PoisonPotion : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
