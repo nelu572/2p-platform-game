817677ae5061f02a664814f4e7b2556c9d69ea2e
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-22 19:57:46 +0900

    feat: 승리 플레이어 가리는 함수 & 죽은 플레이어 부활 함수 작성
    #33

--- Assets/Script/Game/InGameManager.cs
+++ Assets/Script/Game/InGameManager.cs
@@ -1,9 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
     [SerializeField] private PlayerStat Player1;
     [SerializeField] private PlayerStat Player2;
 
+    //일단 작성한것입니다 다시 집가서 다시 수정할 것입니다
+    public void PlayerDead(PlayerStat deadPlayer)
+    {
+        Debug.Log($"player{deadPlayer.TeamId}사망");
+        deadPlayer.Life--;
+        deadPlayer.Hp = deadPlayer.MaxHp;
+        Debug.Log("부활됨");
+    }
 
+    public void SelectWinner(PlayerStat winnerPlayer, PlayerStat loserPlayer)
+    {
+        Debug.Log($"플레이어{winnerPlayer.TeamId} 승리");
+    }
 }
