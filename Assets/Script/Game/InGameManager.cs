dffec1983a18d1ed4fa01318b784ff05b0881d2f
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-25 00:12:11 +0900

    feat: 플레이어의 죽음 감지 추가
    #33

--- Assets/Script/Game/InGameManager.cs
+++ Assets/Script/Game/InGameManager.cs
@@ -5,11 +5,18 @@ public class InGameManager : MonoBehaviour
     [SerializeField] private PlayerStat Player1;
     [SerializeField] private PlayerStat Player2;
 
+    void Update()
+    {
+        if (Player1.Hp <= 0) PlayerDead(Player1);
+        if (Player2.Hp <= 0) PlayerDead(Player2);
+    }
+
     public void PlayerDead(PlayerStat deadPlayer)
     {
         if (deadPlayer.Life > 0)
         {
             Debug.Log($"player{deadPlayer.TeamId}사망");
+            /// TODO: 캐릭터에 맞는 유령 생성 로직 작성 필요 
             deadPlayer.Revive();
             Debug.Log("부활됨");
         }
