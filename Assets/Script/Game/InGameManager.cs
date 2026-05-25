4fd3c4f1467fe518e5a36d814b4369d65974bb7b
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-26 07:48:47 +0900

    feat: 라운드 종료 로직 작성
    #33

--- Assets/Script/Game/InGameManager.cs
+++ Assets/Script/Game/InGameManager.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
     [SerializeField] private PlayerStat Player1;
     [SerializeField] private PlayerStat Player2;
+
+    private bool isRoundOver;
+
 
     void Update()
     {
@@ -31,5 +35,29 @@ public class InGameManager : MonoBehaviour
     public void SelectWinner(PlayerStat winnerPlayer, PlayerStat loserPlayer)
     {
         Debug.Log($"플레이어{winnerPlayer.TeamId} 승리");
+        StartCoroutine(RoundEnd(winnerPlayer, loserPlayer));
+    }
+    private IEnumerator RoundEnd(PlayerStat winner, PlayerStat loser)
+    {
+        isRoundOver = true;
+
+        //  슬로우모션 시작
+        Time.timeScale = 0.2f;        // 속도를 20%로 줄임
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+        // 실제 시간 기준으로 3초 대기 (Unscaled = timeScale 영향 안 받음)
+        yield return new WaitForSecondsRealtime(3f);
+
+        //  속도 원복
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
+
+        // TODO: 결과 UI 표시
+        StartCoroutine(CreateResultUI());
+    }
+    private IEnumerator CreateResultUI()
+    {
+        yield return null;
+        //TODO 결과 화면 SetActive(true) & 결과 화면에 들어가야될 정보 추가
     }
 }
