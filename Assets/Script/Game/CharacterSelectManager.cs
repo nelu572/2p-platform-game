8b634314dbfa61dabe08fb2bf2504eda62ab8172
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-05-29 20:08:32 +0900

    fix: 캐릭터 선택 메니저 수정
    #33

--- Assets/Script/Game/CharacterSelectManager.cs
+++ Assets/Script/Game/CharacterSelectManager.cs
@@ -4,6 +4,35 @@ using UnityEngine.SceneManagement;
 public class CharacterSelectManager : MonoBehaviour
 {
     public CharacterSelectData _characterSelectData;
+    [SerializeField] private UIInput _uiInput;
+
+    private void OnEnable()
+    {
+        _uiInput ??= FindFirstObjectByType<UIInput>();
+
+        if (_uiInput != null)
+            _uiInput.SubmitRequested += OnSubmitRequested;
+    }
+
+    private void OnDisable()
+    {
+        if (_uiInput != null)
+            _uiInput.SubmitRequested -= OnSubmitRequested;
+    }
+
+    private bool OnSubmitRequested(int playerIndex, UIButton button)
+    {
+        CharacterSelectButton selectButton = button.GetComponent<CharacterSelectButton>();
+        if (selectButton == null)
+            return false;
+
+        if (playerIndex == 1)
+            OnPlayer1Select(selectButton.CharacterIndex);
+        else
+            OnPlayer2Select(selectButton.CharacterIndex);
+
+        return true;
+    }
 
     public void OnPlayer1Select(int index) => _characterSelectData.p1CharacterIndex = index;
     public void OnPlayer2Select(int index) => _characterSelectData.p2CharacterIndex = index;
