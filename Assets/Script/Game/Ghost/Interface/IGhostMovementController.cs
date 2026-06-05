e7e654d8d876c173d38f98db58637798f1e3eae5
Jo HyunSung <hunsung1469@gmail.com>
2026-06-05 22:14:50 +0900

    feat: 유령 움직임 컨트롤러 인터페이스 생성
    #46

---
+++ Assets/Script/Game/Ghost/Interface/IGhostMovementController.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// 유령의 자율 이동 구현체가 따라야 하는 계약입니다.
+/// </summary>
+public interface IGhostMovementController
+{
+    public void Initialize(GhostMovementContext context);
+    public void TickMovement(float deltaTime);
+    public void StopMovement();
+}
