72a3300cd4227c99487b43cb4641df2f9144a830
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-08-02 16:24:58 +0900

    fix: 스크립트 이름 변경
    #48

--- Assets/Script/Game/Ghost/Interface/IGhostMovement.cs
+++ Assets/Script/Game/Ghost/Interface/IGhostMovement.cs
@@ -1,7 +1,7 @@
 /// <summary>
 /// 유령의 자율 이동 구현체가 따라야 하는 계약입니다.
 /// </summary>
-public interface IGhostMovementController
+public interface IGhostMovement
 {
     public void Initialize();
     public void TickMovement(float deltaTime);
